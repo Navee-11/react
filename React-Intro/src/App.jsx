81a0d69c536723b1c21928814e1c4fe829e26c26
Navee-11 <naveenkumar.r10692@gmail.com>
2025-01-05 18:01:30 +0530

    React Intro

---
+++ React-Intro/src/App.jsx
@@ -0,0 +1,66 @@
+//Cleaner Form
+// import React from "react";
+
+// function App() {
+//   const [count, setCount] = React.useState(0);
+
+//   return (
+//     <>
+//       <button onClick={() => setCount(count + 1)}>Counter {count}</button>
+//     </>
+//   );
+// }
+
+// export default App
+
+// Functional Form
+// import React from "react";
+
+// function App() {
+//   const [count, setCount] = React.useState(0);
+//   return (
+//     <div>
+//       <Button count={count} setCount={setCount} />
+//     </div>
+//   );
+// }
+
+// function Button(props) {
+//   function buttonClick() {
+//     props.setCount(props.count + 1);
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={buttonClick}>Counter {props.count}</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// Transpiled Form
+import React from "react";
+
+function App() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <Button count={count} setCount={setCount} />
+    </div>
+  );
+}
+
+function Button(props) {
+  function buttonClick() {
+    props.setCount(props.count + 1);
+  }
+
+  return React.createElement(
+    "button",
+    { onClick: buttonClick },
+    "Counter ",
+    props.count
+  );
+}
+export default App;
